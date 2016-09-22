1cc5f894b5513dba06be609538193275e13b6389
Nick Bangia <nick.bangia@gmail.com>
2016-09-21 23:45:34 -0700

    Updated Credentials.sql to use proper case for db name

--- resources/Credentials.sql
+++ resources/Credentials.sql
@@ -1,14 +1,13 @@
 -- CREATE USER STATEMENTS - CREATES THE NEW USER
 -- REPLACE $user WITH DESIRED USERNAME
 -- REPLACE $password WITH DESIRED PASSWORD
--- REPLACE $host WITH THE HOSTNAME OF THE COMPUTER
 CREATE USER '$user'@'localhost' IDENTIFIED BY '$password';
 CREATE USER '$user'@'127.0.0.1' IDENTIFIED BY '$password';
 
 -- GRANT STATEMENTS - GIVES PRIVILEGES TO THE NEW USER
 -- REPLACE $user WITH DESIRED USERNAME FROM ABOVE CREATE USER
 -- REPLACE $host WITH THE HOSTNAME OF THE COMPUTER FROM ABOVE CREATE USER
-GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON familybudget.* TO '$user'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON familybudget.* TO '$user'@'127.0.0.1';
-GRANT DROP ON familybudget.BudgetAllowances TO '$user'@'localhost';
-GRANT DROP ON familybudget.BudgetAllowances TO '$user'@'127.0.0.1';
+GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON FamilyBudget.* TO '$user'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON FamilyBudget.* TO '$user'@'127.0.0.1';
+GRANT DROP ON FamilyBudget.BudgetAllowances TO '$user'@'localhost';
+GRANT DROP ON FamilyBudget.BudgetAllowances TO '$user'@'127.0.0.1';
